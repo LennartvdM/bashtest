bbc352dc493aec1d0f5a7683e785c1e413d30b03
Lennart <lennartvandermolen@gmail.com>
2025-05-01 09:42:56 +0000

    feat: navbar link to sidebar.html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ export default function Navbar() {
           </li>
         ))}
         <li><a href="/Sidebar.html" className="hover:text-white duration-150 sidebar-link">SIDEBAR</a></li>
+        <li><a href="/sidebar.html" className="hover:text-white duration-150">SIDEBAR</a></li>
       </ul>
     </nav>
   );
