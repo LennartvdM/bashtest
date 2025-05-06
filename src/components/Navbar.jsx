f4070e38444753431b6254463cb444e27db193f5
Tokamax <lennartvandermolen@gmail.com>
2025-05-06 14:28:03 +0200

    feat: implement 75% height reduction for hover shift animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,7 +83,7 @@ export default function Navbar() {
       setTraveling(true);
       setBlobHeight(blob?.height || 0);
       setTimeout(() => {
-        setBlobHeight(0);
+        setBlobHeight(blob?.height ? blob.height * 0.75 : 0);
         setTimeout(() => {
           setBlobHeight(blob?.height || 0);
           setTraveling(false);
