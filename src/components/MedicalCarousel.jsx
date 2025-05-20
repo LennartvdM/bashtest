592f1d1b4d5095491ccf3b7025cbd0da83259564
Tokamax <lennartvandermolen@gmail.com>
2025-05-20 17:46:46 +0200

    Trick loading bar: animate to 110% width over 7000ms to appear 10% faster

--- src/components/MedicalCarousel.jsx
+++ src/components/MedicalCarousel.jsx
@@ -286,7 +286,7 @@ function MedicalCarousel({ reverse = false, onSlideChange, onCenterChange, onHig
                         className="absolute left-0 bottom-0 h-[3px]"
                         style={{
                           background: 'rgba(82,156,156,0.5)',
-                          animation: `grow 3000ms linear forwards`,
+                          animation: `grow 7000ms linear forwards`,
                           animationPlayState: isPaused ? 'paused' : 'running',
                           left: -24,
                           width: 100
@@ -349,7 +349,7 @@ function MedicalCarousel({ reverse = false, onSlideChange, onCenterChange, onHig
       </div>
       {/* Keyframes for loading bar */}
       <style>{`
-        @keyframes grow { from { width: 0; } to { width: 100%; } }
+        @keyframes grow { from { width: 0; } to { width: 110%; } }
       `}</style>
     </div>
   );
