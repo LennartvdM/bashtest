1a8e0c78e0193fd46e2866e5e335c73739f295ef
Tokamax <lennartvandermolen@gmail.com>
2025-05-22 14:57:51 +0200

    feat: add MedicalSectionV2 as a fourth scroll section for experimental refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,15 @@ import SkillsSection from '../components/sections/SkillsSection';
 import ProjectsSection from '../components/sections/ProjectsSection';
 import ContactSection from '../components/sections/ContactSection';
 import MedicalSection from '../components/sections/MedicalSection';
+import MedicalSectionV2 from '../components/sections/MedicalSectionV2';
 
 const sections = [
   { name: 'one', component: HeroSection },
   { name: 'two', component: SkillsSection },
   { name: 'three', component: MedicalSection },
-  { name: 'four', component: ProjectsSection },
-  { name: 'five', component: ContactSection },
+  { name: 'four', component: MedicalSectionV2 },
+  { name: 'five', component: ProjectsSection },
+  { name: 'six', component: ContactSection },
 ];
 
 const Home = () => {
