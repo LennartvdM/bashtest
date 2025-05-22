1d4ceeb6220dae638d5e7cf56933015d6b83b7d9
Tokamax <lennartvandermolen@gmail.com>
2025-05-22 17:12:10 +0200

    feat: make video and caption areas flush against centered spacer in MedicalSectionV2

--- src/components/sections/MedicalSectionV2.jsx
+++ src/components/sections/MedicalSectionV2.jsx
@@ -165,20 +165,22 @@ const MedicalSection = ({ inView, sectionRef }) => {
       ))}
       {/* Foreground content: flex row, no card, just video | spacer | captions */}
       <div className="relative z-20 flex flex-row items-center justify-center h-screen w-full">
-        {/* Left: video+band, with yellow outline */}
-        <div className="flex-1 flex justify-end items-center" style={{ outline: '3px solid orange', outlineOffset: '-3px', background: 'transparent', zIndex: 2, minWidth: 0 }}>
-          <MedicalCarousel
-            current={currentVideo}
-            setVideoCenter={setVideoCenter}
-            hoveredIndex={hoveredIndex}
-            isActive={hoveredIndex === currentVideo || isPaused}
-          />
+        {/* Left: video+band, with yellow outline, flush to spacer */}
+        <div className="flex-1 flex justify-end items-center" style={{ outline: '3px solid orange', outlineOffset: '-3px', background: 'transparent', zIndex: 2, minWidth: 0, marginRight: 0, paddingRight: 0 }}>
+          <div style={{ marginRight: 0, paddingRight: 0 }}>
+            <MedicalCarousel
+              current={currentVideo}
+              setVideoCenter={setVideoCenter}
+              hoveredIndex={hoveredIndex}
+              isActive={hoveredIndex === currentVideo || isPaused}
+            />
+          </div>
         </div>
         {/* Spacer: 40px, centered */}
         <div style={{ width: 40, minWidth: 40, height: '80%', outline: '2px solid red', outlineOffset: '-2px', zIndex: 10 }} />
         {/* Right: captions/highlighter, flush to spacer */}
-        <div className="flex-1 flex items-center" style={{ outline: '3px solid orange', outlineOffset: '-3px', minWidth: 0, paddingLeft: 0 }}>
-          <div className="MedicalSection-caption-area flex flex-col items-start justify-center w-full" data-testid="MedicalSection-caption-area" style={{ marginLeft: 0, paddingLeft: 0, position: 'relative' }}>
+        <div className="flex-1 flex items-center" style={{ outline: '3px solid orange', outlineOffset: '-3px', minWidth: 0, paddingLeft: 0, marginLeft: 0, justifyContent: 'flex-start' }}>
+          <div className="MedicalSection-caption-area flex flex-col items-start justify-center w-full" data-testid="MedicalSection-caption-area" style={{ marginLeft: 0, paddingLeft: 0, position: 'relative', maxWidth: 520 }}>
             <div className="relative flex flex-col gap-2 items-start w-full" ref={rightCaptionsRef}>
               {rightReady && Number.isFinite(currentVideo) && Number.isFinite(rightRect.top) && Number.isFinite(rightRect.height) && (
                 <>
