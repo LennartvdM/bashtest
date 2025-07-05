de9270b9d06de6dd59df035f29a211d314409c0e
Tokamax <lennartvandermolen@gmail.com>
2025-07-05 14:40:51 +0200

    Remove 2nd and 3rd scroll sections (SkillsSection and MedicalSection)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import ScrollSnap from '../components/ScrollSnap';
 import SectionManager from '../components/SectionManager';
 import HeroSection from '../components/sections/HeroSection';
-import SkillsSection from '../components/sections/SkillsSection';
 import ProjectsSection from '../components/sections/ProjectsSection';
 import ContactSection from '../components/sections/ContactSection';
-import MedicalSection from '../components/sections/MedicalSection';
 import MedicalSectionV2 from '../components/sections/MedicalSectionV2';
 import MedicalSectionV3 from '../components/sections/MedicalSectionV3';
 
 const sections = [
   { name: 'one', component: HeroSection },
-  { name: 'two', component: SkillsSection },
-  { name: 'three', component: MedicalSection },
-  { name: 'four', component: MedicalSectionV2 },
-  { name: 'five', component: MedicalSectionV3 },
-  { name: 'six', component: ProjectsSection },
-  { name: 'seven', component: ContactSection },
+  { name: 'two', component: MedicalSectionV2 },
+  { name: 'three', component: MedicalSectionV3 },
+  { name: 'four', component: ProjectsSection },
+  { name: 'five', component: ContactSection },
 ];
 
 const Home = () => {
